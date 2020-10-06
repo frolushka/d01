e3d4c2eaa2f0dc79b686d83deadc6463a57c2462
frolushka <mpfrolovstud@gmail.com>
2020-10-06 22:56:51 +0300

    update build

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(target);
+            Destroy(gameObject);
+        }
+    }
+}
